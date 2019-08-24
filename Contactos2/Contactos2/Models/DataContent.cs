64cdbe7216a02c6cb0eb600be9829e463fb3ce01
bryangonca@gmail.com <bryangonca@gmail.com>
2019-08-23 21:00:46 -0400

    Con Entity framework  data content

---
+++ Contactos2/Contactos2/Models/DataContent.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Contactos2.Models
+{
+    using System.Data.Entity;
+
+    public class DataContent:DbContext
+    {
+        public DataContent():base("DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ Contactos2/Contactos2/Models/contact.cs
@@ -0,0 +1,25 @@
+﻿using Contactos2.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Contactos2.Models
+{
+    public enum Typecontact
+    {
+        telephone,
+        email,
+        facebook
+    }
+
+    public class contact
+    {
+    [Key]
+    private int contactID { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public Typecontact type { get; set; }
+    [Required]
+    [Display(Description ="contact")]
+    public string contactvalue { get; set; }
+}
+}
